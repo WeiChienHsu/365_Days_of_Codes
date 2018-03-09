d021726249d74fc5f978771b22bf416eeb407cc0
WeiChien Hsu <weichien711@gmail.com>
2018-03-09 15:58:43 -0600

    :hammer: Add SearchBar Component (with EventHandler)

--- React/ReduxSimple/src/components/search_bar.js
+++ React/ReduxSimple/src/components/search_bar.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class SearchBar extends React.Component{
+class SearchBar extends Component{
+  constructor(props) {
+    super(props);
+    this.state = { term: '' };
+    this.showWord = this.showWord.bind(this);
+  }
+
   render() {
     return(
-      <input />
+      <div>
+      <input 
+        value = {this.state.term}
+        onChange = {event => this.setState({term : event.target.value})} />
+      <button onClick = {this.showWord}> Click me! </button>
+      </div>
     )
   }
+
+  showWord() {
+    const new_words = this.state.term;
+    console.log(new_words);
+  }
+
 }
 
 export default SearchBar;
