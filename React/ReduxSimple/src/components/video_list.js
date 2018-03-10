febf43cda692449821be3305ada8a50256b6ea82
WeiChien Hsu <weichien711@gmail.com>
2018-03-10 13:22:14 -0600

    :construction: Add Vidoe Detail and iframe to the youtube video by embed Youtube

--- React/ReduxSimple/src/components/video_list.js
+++ React/ReduxSimple/src/components/video_list.js
@@ -7,7 +7,7 @@ const VideoList = (props) => {
   });
 
   return(
-    <ul className = "col-lg-12  list-group" >
+    <ul className = "col-md-8  list-group" >
       {videoItems}
     </ul>
   );
--- React/ReduxSimple/src/index.js
+++ React/ReduxSimple/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/vedio_detail';
 
 const API_KEY = "AIzaSyBJStuWHujWCV9kTWgYUy3YOL87Ea0YtQc";
 
@@ -20,7 +21,9 @@ class App extends React.Component{
     return(
       <div>
         <SearchBar/>
+        <VideoDetail video = {this.state.videos[0]}/>
         <VideoList videos = {this.state.videos}/>
+
       </div>
     );
   }
