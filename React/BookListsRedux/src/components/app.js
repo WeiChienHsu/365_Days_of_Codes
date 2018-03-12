a5902e569cf203bb1fdcc90db0805edf6b318c32
WeiChien Hsu <weichien711@gmail.com>
2018-03-12 12:55:32 -0500

    :construction: Create Book Detail Container

--- React/BookListsRedux/src/components/app.js
+++ React/BookListsRedux/src/components/app.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 
 import BookList from '../containers/book-list';
+import BookDetail from '../containers/book-detail';
 
 class App extends Component{
   render() {
     return(
     <div>
       <BookList />
+      <BookDetail />
     </div>
     )}
 }
---
+++ React/BookListsRedux/src/containers/book-detail.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class BookDetail extends Component{
+    render() {
+      return (
+        <div>Book Detail! </div>
+      )
+    }
+}
